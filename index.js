49f8fbdc2d1e0957fcad9efdebd0db9ae77a223d
duralog <funisher@gmail.com>
2014-07-14 06:41:40 +0200

    whoops, _config.Auth as well...

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var tmp      = osenv.tmpdir()
 function dumpConfig (config) {
   var _config = JSON.parse(JSON.stringify(config))
   if (_config.credentials) _config.credentials.pass = _config.credentials.pass.replace(/./g,'*')
-  _config.Auth = _config.Auth.replace(/./g, '*')
+  if (_config.Auth) _config.Auth = _config.Auth.replace(/./g, '*')
   console.log(JSON.stringify(_config, null, 2))
 }
 
